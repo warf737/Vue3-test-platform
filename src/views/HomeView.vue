5cf176aedcc3e29aa0a586ded1434e84a993ff93
dvasilchuk <nuvolanam@gmail.com>
2026-02-05 19:51:10 +0300

    Setup eslint and prettier

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,18 +10,18 @@
         <h2>Vue 3 + Composition API + Pinia + TypeScript</h2>
 
         <el-divider />
-        
+
         <h3>Пример использования Pinia Store:</h3>
-        <div style="margin: 20px 0;">
-          <el-button @click="counterStore.decrement" :icon="Minus" circle />
-          <span style="margin: 0 20px; font-size: 24px;">{{ counterStore.count }}</span>
-          <el-button @click="counterStore.increment" :icon="Plus" circle />
+        <div style="margin: 20px 0">
+          <el-button :icon="Minus" circle @click="counterStore.decrement" />
+          <span style="margin: 0 20px; font-size: 24px">{{ counterStore.count }}</span>
+          <el-button :icon="Plus" circle @click="counterStore.increment" />
         </div>
-        
+
         <el-divider />
-        
+
         <h3>Пример формы Element Plus:</h3>
-        <el-form :model="form" label-width="120px" style="max-width: 500px;">
+        <el-form :model="form" label-width="120px" style="max-width: 500px">
           <el-form-item label="Имя">
             <el-input v-model="form.name" />
           </el-form-item>
@@ -29,7 +29,7 @@
             <el-input v-model="form.email" type="email" />
           </el-form-item>
           <el-form-item>
-            <el-button type="primary" @click="onSubmit">Отправить</el-button>
+            <el-button type="primary" @click="onSubmit">Сохранить</el-button>
             <el-button @click="onReset">Сбросить</el-button>
           </el-form-item>
         </el-form>
@@ -42,13 +42,15 @@
 import { reactive } from 'vue'
 import { ElMessage } from 'element-plus'
 import { Plus, Minus } from '@element-plus/icons-vue'
-import { useCounterStore } from '@/stores/accounts.ts'
+import { useAccountsStore } from '@/stores/accounts.ts'
 
-const counterStore = useCounterStore()
+const counterStore = useAccountsStore()
 
 const form = reactive({
-  name: '',
-  email: ''
+  label: '',
+  type: '',
+  login: '',
+  password: '',
 })
 
 const onSubmit = () => {
